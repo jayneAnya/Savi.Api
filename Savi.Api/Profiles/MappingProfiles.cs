ecdedf2dd970fb90fe05ceaf0b1dfd010d8b2cc2
Jane Anyanwu <jane.anyanwu@maxfront.com>
2025-08-05 23:41:49 +0100

    fixed build error

--- Savi.Api/Profiles/MappingProfiles.cs
+++ Savi.Api/Profiles/MappingProfiles.cs
@@ -21,6 +21,7 @@ namespace Savi.Api.Profiles
 			CreateMap<GroupSavingsMembers, GroupMembersDto>().ReverseMap();
 			CreateMap<SetTarget, UpdatSetTargetDTO>().ReverseMap();
 			CreateMap<SetTargetFunding, SetTargetFundingDTO>().ReverseMap();
-      CreateMap<KYC, AddKycDto>().ReverseMap();
+			CreateMap<KYC, AddKycDto>().ReverseMap();
+		}
 	}
 }
